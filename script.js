3f12934a21875c46567aa3510e2c38b67e386b8f
carlos 'tuto' curiel <ccurielramos@gmail.com>
2025-01-20 17:13:04 +0100

    Agregada funcion para los espacios de adivinar letra e intentos

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 "use strict";
 
-
 const listaDePalabras = ["botella", "escritorio", "paraguas", "chimpance"];
+const palabraOcultaDiv = document.getElementById("palabraOculta");
+const intentosRestantesDiv = document.getElementById("intentosRestantes");
 
 let palabraAdivinar = [];
 let palabraMostrar = [];
 let logLetrasUser = [];
-let numIntentos = 6; //por decir un numero
+let intentosRestantes = 6;
 
 function prepJuego() {
   let pickListaPalabras = Math.floor(Math.random() * listaDePalabras.length);
   console.log(pickListaPalabras);
-  let palabraAleatoria = listaDePalabras[pickListaPalabras];
+  const palabraAleatoria = listaDePalabras[pickListaPalabras].toUpperCase();
   console.log(palabraAleatoria);
+  let palabraAdivinar = palabraAleatoria.split("");
+  let letrasCorrectas = Array(palabraAdivinar.length).fill("");
+  console.log(letrasCorrectas);
+  mostrarLineas(palabraAdivinar);
+  actualizarIntentos();
+}
+prepJuego();
 
-  palabraAdivinar = palabraAleatoria.split("");
+function mostrarLineas(palabra) {
+  palabraOcultaDiv.innerHTML = "";
+  palabra.forEach(() => {
+    const letraDiv = document.createElement("div");
+    letraDiv.classList.add("letra");
+    letraDiv.textContent = "";
+    palabraOcultaDiv.appendChild(letraDiv);
+  });
 }
 
+function actualizarIntentos() {
+  intentosRestantesDiv.textContent = `Intentos restantes: ${intentosRestantes}`;
+}
